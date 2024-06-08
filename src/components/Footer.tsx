09d2e42c872b9d7c8826f8d4fd6246c6cbb00b57
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-06-08 17:52:40 +0900

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="text-center py-2 bg-black">
+      <p className="text-white">
+        Don't forget to CODE your DREAM | All Rights Reserved.
+      </p>
+    </footer>
+  );
+}

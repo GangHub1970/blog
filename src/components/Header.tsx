60b2f57a0622b826b2b30c60aa14180c1118f1b3
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2024-06-08 17:35:48 +0900

    Header(navbar, logo)

---
+++ src/components/Header.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+import { Roboto } from "next/font/google";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+
+const roboto = Roboto({
+  weight: "700",
+  subsets: ["latin"],
+});
+
+export default function Header() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleLogoClick = () => {
+    router.push("/");
+  };
+  return (
+    <header className="flex justify-between items-center py-4 px-8">
+      <h1
+        className={`text-2xl font-bold cursor-pointer ${roboto.className}`}
+        onClick={handleLogoClick}
+      >
+        Gang's Blog
+      </h1>
+      <nav className="flex gap-4">
+        <Link className={`${pathname === "/" ? "text-blue-500" : ""}`} href="/">
+          home
+        </Link>
+        <Link
+          className={`${pathname === "/about" ? "text-blue-500" : ""}`}
+          href="/about"
+        >
+          about
+        </Link>
+        <Link
+          className={`${pathname === "/posts" ? "text-blue-500" : ""}`}
+          href="/posts"
+        >
+          posts
+        </Link>
+        <Link
+          className={`${pathname === "/contact" ? "text-blue-500" : ""}`}
+          href="/contact"
+        >
+          contact
+        </Link>
+      </nav>
+    </header>
+  );
+}
